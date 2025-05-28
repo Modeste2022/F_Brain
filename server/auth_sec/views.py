3abb3fddfeacdb4e763afd1073a4f40e7fcaeff2
modeste2022 <landry.monthe@facsciences-uy1.cm>
2025-05-28 19:33:32 +0200

    Api Django fonctionnel

--- server/auth_sec/views.py
+++ server/auth_sec/views.py
@@ -1,4 +1,4 @@
 from django.http import JsonResponse
 
 def api_home(request):
-    return JsonResponse({"message": "Bienvenue sur l'API du serveur. Ceci est projet d'authentification"})
+    return JsonResponse({"message": "Bienvenue sur l'API du serveur. Ceci est un projet d'authentification et de sécurité"})
